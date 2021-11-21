fe90d81f501667ec7e3e585f1554c46940be9d5a
its_d <dineshparadesi94@gmail.com>
2021-11-21 20:54:26 +1000

    Updated port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, './assets')));
 app.set('view engine', 'ejs');
 routes(app);
 
-const port = process.env.PORT || 8083;
+const port = process.env.PORT || 8080;
 
 app.listen(port);
 console.log(`Yo server is up @ ${port}`);
